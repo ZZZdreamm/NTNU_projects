b0afc60c216a7bc5eacb6f520051e23d88fc7061
ajonczyk <01178532@pw.edu.pl>
2024-01-24 21:56:39 +0100

    Pong game added

--- software_architecture/assigment_1/pong/ios/src/com/mygdx/game/IOSLauncher.java
+++ software_architecture/assigment_1/pong/ios/src/com/mygdx/game/IOSLauncher.java
@@ -5,13 +5,13 @@ import org.robovm.apple.uikit.UIApplication;
 
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
-import com.mygdx.game.Pong;
+import com.mygdx.game.MyGdxGame;
 
 public class IOSLauncher extends IOSApplication.Delegate {
     @Override
     protected IOSApplication createApplication() {
         IOSApplicationConfiguration config = new IOSApplicationConfiguration();
-        return new IOSApplication(new Pong(), config);
+        return new IOSApplication(new MyGdxGame(), config);
     }
 
     public static void main(String[] argv) {
